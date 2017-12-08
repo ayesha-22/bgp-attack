dffac4eaa5391a43ddc3de0f92288bf515b81e04
carolinesimas <carol.simas92@gmail.com>
2017-12-08 16:39:06 -0300

    Update stop_rogue.sh

--- stop_rogue.sh
+++ stop_rogue.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo python run.py --node R10 --cmd "pgrep -f [z]ebra-R10 | xargs kill -9"
-sudo python run.py --node R10 --cmd "pgrep -f [b]gpd-R10 | xargs kill -9"
+sudo python run.py --node R3 --cmd "pgrep -f [z]ebra-R3 | xargs kill -9"
+sudo python run.py --node R3 --cmd "pgrep -f [b]gpd-R3 | xargs kill -9"
