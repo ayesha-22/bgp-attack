ab079db90e2ce6f7d599f4522276bdbe1050ab46
carolinesimas <carol.simas92@gmail.com>
2017-12-08 16:40:14 -0300

    Update website.sh

--- website.sh
+++ website.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-node=${1:-h1-1}
+node=${4:-h4-1}
 bold=`tput bold`
 normal=`tput sgr0`
 
 while true; do
-    out=`sudo python run.py --node $node --cmd "curl -s 6.0.1.1"`
+    out=`sudo python run.py --node $node --cmd "curl -s 8.0.1.1"`
     date=`date`
     echo $date -- $bold$out$normal
     sleep 1
