4d59a2ebf76db670f08fca7f462ca7fa24a0ce7d
carolinesimas <carol.simas92@gmail.com>
2017-12-09 14:45:28 -0300

    Update start_rogue.sh

--- start_rogue.sh
+++ start_rogue.sh
@@ -4,5 +4,5 @@ echo "Killing any existing rogue AS"
 ./stop_rogue.sh
 
 echo "Starting rogue AS"
-sudo python run.py --node R3 --cmd "/usr/lib/quagga/zebra -f conf/zebra-R3.conf -d -i /tmp/zebra-R3.pid > logs/R3-zebra-stdout"
-sudo python run.py --node R3 --cmd "/usr/lib/quagga/bgpd -f conf/bgpd-R3.conf -d -i /tmp/bgpd-R3.pid > logs/R3-bgpd-stdout"
+sudo python run.py --node R3 --cmd "/usr/lib/quagga/zebra -f conf/zebra-R10.conf -d -i /tmp/zebra-R10.pid > logs/R10-zebra-stdout"
+sudo python run.py --node R3 --cmd "/usr/lib/quagga/bgpd -f conf/bgpd-R10.conf -d -i /tmp/bgpd-R10.pid > logs/R10-bgpd-stdout"
