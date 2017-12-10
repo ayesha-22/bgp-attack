b4a86cd680c6fc4e02bbd398acb9e45737bb93dd
carolinesimas <carol.simas92@gmail.com>
2017-12-10 11:53:14 -0300

    bgp-attack

--- bgp.py
+++ bgp.py
@@ -90,6 +90,7 @@ class SimpleTopo(Topo):
         self.addLink('R3', 'R4')
         self.addLink('R4', 'R5')
         self.addLink('R4', 'R6')
+        self.addLink('R4', 'R7')
         self.addLink('R5', 'R7')
         self.addLink('R6', 'R7')
         self.addLink('R7', 'R8')
